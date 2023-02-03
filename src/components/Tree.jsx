2bc9f7af5c0167749c06b7424d643f929680a717
yardenedry <yardenedry0@gmail.com>
2023-02-03 17:41:10 -0500

    zoom/pan implementation onto tree comp

--- src/components/Tree.jsx
+++ src/components/Tree.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import Errors from './Errors'
+import * as d3 from 'd3'
 
 
 const Tree = (props) => {
   console.log('props from tree', props)
+
+  const handleZoom = (e) => { d3.select('chart').attr('transform, e.transform') }
+  const zoom = d3.zoom().on('zoom', handleZoom) 
+  d3.select('chart').call(zoom)
 
   return (
     <div id='tree-div'>
