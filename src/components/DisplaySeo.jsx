aef07a868ed4ca48617ac5764966f73fdd6ba184
yardenedry <yardenedry0@gmail.com>
2023-02-04 19:03:12 -0500

    score wheels implemented under performance UI

--- src/components/DisplaySeo.jsx
+++ src/components/DisplaySeo.jsx
@@ -1,5 +1,8 @@
+import { fontSize } from "@mui/system";
 import React, {useState} from "react";
 import { useEffect } from "react";
+import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
 
 export default function DisplaySeo () {
   const [url, setUrl] = useState('')
@@ -36,15 +39,72 @@ export default function DisplaySeo () {
   // }, [lighthouseData]);
 
    console.log(lighthouseData);
+   const testVal1 = 94;
+   const testVal2 = 50;
+   const testVal3 = 22;
+
+   const filledStyle = {
+    backgroundColor: '#3F51B5',
+    textColor: '#fff',
+    textSize: '24px',
+    pathColor: '#fff',
+    trailColor: 'transparent'
+  }
 
   return (
-    <div>
+    <div id="seo-div">
       <button onClick={runLighthouse}>Run lighthouse</button>
       <p>{url && url[0].url}</p>
-
       {lighthouseData.categories ? <p>{lighthouseData.categories.seo.score}</p> : null}
+      <h2 id='h2-scores'>Performance Scores:</h2>
+      <div id="seo-wheels">
+        <div id="seo-val-and-title">
+          <div id="seo-val-wrap">
+            <CircularProgressbar value={testVal1} text={`${testVal1}`} counterClockwise
+            background backgroundPadding={6}
+            styles={buildStyles(filledStyle)} />
+          </div>
+          <p>Speed</p>
+        </div>
 
+        <div id="seo-val-and-title">
+          <div id="seo-val-wrap">
+            <CircularProgressbar value={testVal2} text={`${testVal2}`} counterClockwise
+            background backgroundPadding={6}
+            styles={buildStyles(filledStyle)} />
+          </div>
+          <p>SEO</p>
+        </div>
+
+        <div id="seo-val-and-title">
+          <div id="seo-val-wrap">
+            <CircularProgressbar value={testVal3} text={`${testVal3}`} counterClockwise
+            background backgroundPadding={6}
+            styles={buildStyles(filledStyle)} />
+          </div>
+          <p>Test</p>
+        </div>
+      </div>
     </div>
   )
 
 };
+
+
+/**
+ *  <div id="seo-val-wrap">
+          <div id="seo-val-outer">
+            <div id="seo-val-inner">{testVal1}</div>
+          </div>
+
+          <svg id='seo-svg' xmlns="http://www.w3.org/2000/svg" version="1.1" width="50px" height="50px">
+            <defs>
+              <linearGradient id="GradientColor">
+                <stop offset="0%" stop-color="#e91e63" />
+                <stop offset="100%" stop-color="#673ab7" />
+              </linearGradient>
+            </defs>
+            <circle cx="26" cy="26" r="20" stroke-linecap="round" />
+          </svg>
+        </div>
+ */
